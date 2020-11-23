a039b4cbf054002dcf798ff9a9f4bd4258b4d6db
policeswat45 <74921861+policeswat45@users.noreply.github.com>
2020-11-23 23:27:43 +0700

    Add files via upload

--- numberSpeculation.py
+++ numberSpeculation.py
@@ -20,29 +20,32 @@ def main_game ():
         while option > 2:
             print("Unable to complie")
             option = int(input("Type 1 to play or 2 to end "))
+
             if option <= 2:
                 break
 
         if option == 2:
             break
+
         playerNumber = int (input ("What is your verdict? (From 1 to 11) "))
-        while playerNumber > 11 or playerNumber < 1:
+        while playerNumber < 1 or playerNumber > 1:
             print ("Please only select the number from 1 to 11")
             playerNumber = int (input("What is your verdict? (From 1 to 11) "))
 
         machineNumber = random.randint(1,11)
-
         print ("The number of Machine is ",  machineNumber)
         print("The number of Player is ",  playerNumber)
+
         if playerNumber ==  machineNumber:
 
            print ("You won!")
 
         else:
+
             print ("The machine won!")
 
         input("Press Enter to continue...")
-        #sleep(1)         #You can add sleep function here to slow it down 
-        os.system('cls')
+        #sleep(1)         #You can add sleep function here to slow it down
+        os.system('cls')  #Clear screen function; you need to turn on "Emulate terminal" in Run/Edit Config
 
 main_game()
